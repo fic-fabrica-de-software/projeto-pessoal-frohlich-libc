29c382d8d0803d542da2df77c2c6bc5eb3700e6d
frohlich-pedro <pedro_h_nf@outlook.com>
2025-08-21 14:21:45 -0300

    update

--- src/rand.s
+++ src/rand.s
@@ -4,16 +4,20 @@
 .global	rand
 .type	rand,	@function
 rand:
-	push	%rbp
-	mov	%rsp,	%rbp
+	push    %rbp
+	mov %rsp,   %rbp
 
-	mov	seed(%rip),	%rax
-	mov	$1103515245,	%rdi
-	mul	%rdi
-	add	$12345,	%rax
-	and	$0x7fffffff,	%rax
+	mov	seed(%rip),	%r12
+	mov	multiplier(%rip),	%r13
+	mov	increment(%rip),	%r14
+	mov	mod(%rip),	%r15
+
+	mov	%r12,	%rax
+	mul	%r13
+	add	%r14,	%rax
+	and	%r15,	%rax
 	mov	%rax,	seed(%rip)
-
+	
 	mov	%rbp,	%rsp
 	pop	%rbp
 	ret
@@ -21,3 +25,6 @@ rand:
 .section	.data
 
 seed:	.quad	1234566789
+multiplier:	.quad   1103515245
+increment:	.quad   12345
+mod:	.quad   0x7fffffff
